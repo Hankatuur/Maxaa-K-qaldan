18a915950d97fe027b46e8de20e465858202af95
Hankatuur <165064976+Hankatuur@users.noreply.github.com>
2025-03-10 09:27:41 +0300

    Fixed vercel error as i think

--- src/components/shared/SearchResult.tsx
+++ src/components/shared/SearchResult.tsx
@@ -1,26 +1,74 @@
 import { Models } from 'appwrite';
-import React from 'react'
+import React from 'react';
 import Loader from './loader';
 import GridPostList from './GridPostList';
- type SearchResultProps = {
-  isSearchFetching :boolean;
-  searchedPosts: Models.Document[];
 
- }
-const SearchResult = ({isSearchFetching,searchedPosts}:SearchResultProps) => {
-  if(isSearchFetching) {
-    return ( 
-    <Loader/>
-  )
+type SearchResultProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.DocumentList; // Ensure the correct type
+};
+
+const SearchResult = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
+  if (isSearchFetching) {
+    return <Loader />;
   }
 
-  if(searchedPosts&& searchedPosts.documents.length > 0) {
-     return (
-  <GridPostList posts={searchedPosts.documents}/>)
-     }
-  return (
-     <p className='text-light-4 mt-10 text-center w-full'>No Results Found</p>
-  )
-}
+  if (searchedPosts && searchedPosts.documents.length > 0) { // Fix here
+    return <GridPostList posts={searchedPosts.documents} />; // Fix here
+  }
 
-export default SearchResult
+  return <p className='text-light-4 mt-10 text-center w-full'>No Results Found</p>;
+};
+
+export default SearchResult;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Models } from 'appwrite';
+// import React from 'react'
+// import Loader from './loader';
+// import GridPostList from './GridPostList';
+//  type SearchResultProps = {
+//   isSearchFetching :boolean;
+//   searchedPosts: Models.DocumentList[];
+
+//  }
+// const SearchResult = ({isSearchFetching,searchedPosts}:SearchResultProps) => {
+//   if(isSearchFetching) {
+//     return ( 
+//     <Loader/>
+//   )
+//   }
+
+//   if(searchedPosts&& searchedPosts.documents.length > 0) {
+//      return (
+//   <GridPostList posts={searchedPosts.documents}/>)
+//      }
+//   return (
+//      <p className='text-light-4 mt-10 text-center w-full'>No Results Found</p>
+//   )
+// }
+
+// export default SearchResult
